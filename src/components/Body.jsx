fe24867484a563f034abf6c75fc29e8f73df85c0
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-12-28 15:51:53 +0000

    Updated Body

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,7 +1,24 @@
 function Body() {
   return (
     <>
-      <div>Body belongs here</div>
+      <section className="section_body">
+        <h2 className="sec_body_heading1">About</h2>
+        <p className="sec_body_para1">
+          Hello! <br />I am Valay!, a 2025 CSE graduate having interest in
+          Cybersecurity, Software Engineering and Web Development. I try to
+          learn whatever I can and because of that most of the time my things
+          are incomplete, But all of my things will be completed one or the
+          other day.
+        </p>
+        <h2 className="sec_body_heading2">Interests</h2>
+        <p className="sec_body_para2">
+          I mostly like playing Video Games, Badminton, doing CTFs, Travelling,
+          watching different genres of movies, web series, documentaries, (my
+          closest to favourite genre is found footages) so if you have any in
+          mind you can ping me 😙. I have many more things as interest but I
+          just can add all that here.
+        </p>
+      </section>
     </>
   );
 }
